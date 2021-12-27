177aafad252fb543932dd00c2550c3e5413a4f4c
Riley Castelli (she/her) <28277602+n-tropy247@users.noreply.github.com>
2021-12-27 18:00:31 -0500

    Update aliases.zsh

--- .zshrc-custom/aliases.zsh
+++ .zshrc-custom/aliases.zsh
@@ -1,4 +1,4 @@
-" Provided by Riley Castelli and intended for use on stdlinux at OSU
+#Provided by Riley Castelli and intended for use on stdlinux at OSU
 alias yd="pwd | tr -d '\n' | xsel -i -b"
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
