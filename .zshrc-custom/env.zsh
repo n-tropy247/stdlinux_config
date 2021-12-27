fc5a6dfab6454115ad370eef3d2692b1e7a71c1c
Riley Castelli (she/her) <28277602+n-tropy247@users.noreply.github.com>
2021-12-27 18:01:09 -0500

    Update env.zsh

--- .zshrc-custom/env.zsh
+++ .zshrc-custom/env.zsh
@@ -1,3 +1,4 @@
+#Provided by Riley Castelli and intended for use on stdlinux at OSU
 [ -f ~/.fzf.zsh  ] && source ~/.fzf.zsh
 fpath=("$HOME/.zfunctions" $fpath)
 export VISUAL="$HOME/.local/bin/nvim"
