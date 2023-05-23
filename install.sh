1c86f461516eec4e349de8f4e4c4fad18f2f364a
Riley Castelli <castelliriley@gmail.com>
2023-05-23 16:45:23 -0400

    reordered install script again

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ rm -rf $HOME/.zshrc
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.tmux
 rm -rf $HOME/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
@@ -15,5 +14,6 @@ mv nvim.appimage nvim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp $HOME/stdlinux_config/.zshrc $HOME/.zshrc
 cp $HOME/stdlinux_config/.bashrc $HOME/.bashrc
-cp -r $HOME/stdlinux_config/.config $HOME/.config
+cp -r $HOME/stdlinux_config/.config/* $HOME/.config/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source $HOME/.bashrc
