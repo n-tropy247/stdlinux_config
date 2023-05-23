4c2e2c700642123fd73dbd713602ebba05c90fb7
Riley Castelli <castelliriley@gmail.com>
2023-05-23 17:29:48 -0400

    added rust binary for LSP building

--- install.sh
+++ install.sh
@@ -5,13 +5,21 @@ rm -rf $HOME/.local/nvim
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.tmux
 rm -rf $HOME/.oh-my-zsh
+mkdir $HOME/.rust
+cd $HOME/.rust
+wget https://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz
+tar xf rust-nightly-x86_64-unknown-linux-gnu.tar.gz
+cd $HOME
+mv rust-nightly-x86_64-unknown-linux-gnu rust
 unset ZSH
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage nvim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/n-tropy247/astronvim_config.git ~/.config/nvim/lua/user
 cp $HOME/stdlinux_config/.bashrc $HOME/.bashrc
 cp -r $HOME/stdlinux_config/.config/* $HOME/.config/
+cp -r $HOME/stdlinux_config/.local/* $HOME/.local/
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
